e7d7a3d510782d3063d0f91bafa9f9f2bdf53fc7
planzjd <86262970+planzjd@users.noreply.github.com>
2021-06-22 15:30:56 -0500

    Josh's updates to changes

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -7,14 +7,17 @@ namespace communication_log
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Astronaut!");
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Hello Control");
             Console.WriteLine("Are you flying today?");
             Console.WriteLine("Houston waiting for count down");
             Console.WriteLine("Houston push update");
             Console.WriteLine("Still Waiting!!");
-            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.Gray;
             Console.WriteLine("Why do men die before their wife's...?");
+            Console.WriteLine("Line 1 Josh");
+            Console.WriteLine("Line 2 Josh");
+            Console.WriteLine("Line 3 Josh...");
             Console.Read();
 
         }
