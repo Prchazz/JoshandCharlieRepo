fb9ac03a3fd08ace3f16fbe73f462f1f3e72a4c7
Charlie <Charlie@carde-macbook.myfiosgateway.com>
2021-06-22 16:31:30 -0400

    added colors and 3 lines of text

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -7,14 +7,17 @@ namespace communication_log
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Astronaut!");
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Hello Control");
             Console.WriteLine("Are you flying today?");
             Console.WriteLine("Houston waiting for count down");
             Console.WriteLine("Houston push update");
             Console.WriteLine("Still Waiting!!");
-            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("Why do men die before their wife's...?");
+            Console.WriteLine("Because men want too.");
+            Console.WriteLine("Charles jacked it up!");
+            Console.WriteLine("Josh jacked it up!");
             Console.Read();
 
         }
