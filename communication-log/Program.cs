97729e7800d6ae304a7bc071480c5eef8e724610
planzjd <86262970+planzjd@users.noreply.github.com>
2021-06-22 15:20:11 -0500

    sending to control

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -6,16 +6,17 @@ namespace communication_log
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Astronauts!");
+            Console.WriteLine("Hello Astronaut!");
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.WriteLine("Hello Control");
             Console.WriteLine("Are you flying today?");
-            Console.WriteLine("Houston watching for count down");
+            Console.WriteLine("Houston waiting for count down");
             Console.WriteLine("Houston push update");
             Console.WriteLine("Still Waiting!!");
-            Console.WriteLine("Hold your horses and watch your six, update coming.");
-            Console.WriteLine("Any luck?");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Why do men die before their wife's...?");
             Console.Read();
+
         }
     }
 }
